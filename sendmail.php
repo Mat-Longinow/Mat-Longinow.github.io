e2a5822730ac16e72e2924905b7d0643f3c85b9d
Mat-Longinow <mat.longinow@gmail.com>
2018-01-28 15:14:41 -0800

    Secrure email change in Form to Email

--- sendmail.php
+++ sendmail.php
@@ -3,6 +3,6 @@
 	$email = $_REQUEST['email'];
 	$message = $_REQUEST['message'];
 
-	mail("mat.longinow@gmail.com", "MyProgrammingJourney Form Results", $message, "From: $name at $email");
+	mail("mat.longinow@outlook.com", "MyProgrammingJourney Form Results", $message, "From: $name at $email");
 	// header("Location: http://www.myprogrammingjourney.com/thankyou.html");
 ?>
