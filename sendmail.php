09dd27cef9bf72fa1ffe00a6b71912d353eb2493
Mat-Longinow <mat.longinow@gmail.com>
2018-01-30 22:02:37 -0800

    Thank You Page

--- sendmail.php
+++ sendmail.php
@@ -4,5 +4,5 @@
 	$message = $_REQUEST['message'];
 
 	mail("mat.longinow@outlook.com", "MyProgrammingJourney Form Results", $message, "From: $name at $email");
-	// header("Location: http://www.myprogrammingjourney.com/thankyou.html");
+	header("Location: http://www.myprogrammingjourney.com/thankyou.html");
 ?>
